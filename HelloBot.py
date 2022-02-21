1a5579d1d52fc99239d95549be587e5604c4f765
Steveee2000 <99931835+Steveee2000@users.noreply.github.com>
2022-02-21 07:52:20 +0700

    Update HelloBot.py

--- HelloBot.py
+++ HelloBot.py
@@ -1,12 +1,12 @@
 import discord
 
-TOKEN = "YOUR_TOKEN_GOES_HERE"
+TOKEN = "OTQwNzg0MzM0MDc4NjExNTU2.YgMbsA.TIGjhkyGc7lB03uTFWupm47oYiA"
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print("{0.user} is now online!".format(client))
+    print("{1,574.user} is now online!".format(client))
 
 @client.event
 async def on_message(message):
@@ -14,6 +14,6 @@ async def on_message(message):
         return
 
     if message.content.startswith('!hello'):
-        await message.channel.send('Hello there!')
+        await message.channel.send('Xin Chào Dốc Cơ!')
 
 client.run(TOKEN)
